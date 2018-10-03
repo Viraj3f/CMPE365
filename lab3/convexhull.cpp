843a9debfa5490827d3f7242c643931ada571f4f
Viraj Bangari <Viraj3f@gmail.com>
2018-10-03 09:51:18 -0400

    Finish lab3

--- lab3/convexhull.cpp
+++ lab3/convexhull.cpp
@@ -41,12 +41,19 @@ std::ostream& operator<<(std::ostream& io, Point& s)
     return io;
 }
 
+/*
+ * Determines wheter point c is found by going clockwise from
+ * line ab.
+ */
 bool isCW(Point& a, Point& b, Point& c)
 {
     int ans = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
     return ans > 0;
 }
 
+/*
+ * Finds a convex hull in a set of n points
+ */
 vector<Point*> grahmScan(vector<Point>& points)
 {
     // Sort points by y value
@@ -85,6 +92,11 @@ vector<Point*> grahmScan(vector<Point>& points)
     return st;
 }
 
+
+/*
+ * Checks if two convex hulls interect by using bounding circles.
+ * Can sometimes create false positives
+ */
 bool doConvexHullsIntersect(vector<Point>& l, vector<Point>& r)
 {
     Point lMean;
@@ -123,6 +135,103 @@ bool doConvexHullsIntersect(vector<Point>& l, vector<Point>& r)
 
     return rDist + lDist >= lMean.calculateDist(rMean);
 }
+
+/*
+ * For a line that passes through x and y, return 1 is z is 
+ * above the line, -1 if it is below or 0 if it is tangent.
+ */
+int getTangentDirection(Point& a, Point& b, Point& z)
+{
+    int det = a.x * b.y - a.x * z.y
+            - a.y * b.x + a.y * z.x
+            + b.x * z.y - b.y * z.x;
+
+    if (det == 0)
+    {
+        return det;
+    }
+
+    return det;
+}
+
+/*
+ * Checks if two convex hulls interect by using a separating line.
+ * Assumes the two convex hulls l and r have their points in increasingly
+ * monotomically in either the CW or CCW direction.
+ */
+bool doConvexHullsIntersectGuaranteed(vector<Point>& l, vector<Point>& r)
+{
+    Point lMean;
+    for (Point& p : l)
+    {
+        lMean.x += p.x;
+        lMean.y += p.y;
+    }
+    lMean.x /= l.size();
+    lMean.y /= l.size();
+
+    Point rMean;
+    for (Point& p : r)
+    {
+        rMean.x += p.x;
+        rMean.y += p.y;
+    }
+    rMean.x /= r.size();
+    rMean.y /= r.size();
+
+    if (lMean.x > rMean.x)
+    {
+        swap(l, r);
+    }
+
+    for (int i = 0; i < l.size() - 1; i++)
+    {
+        Point& a = l[i];
+        Point& b = l[(i + 1) % l.size()];
+        int counter = 0;
+        for (Point& z : r)
+        {
+            if (getTangentDirection(a, b, z) <= 0)
+            {
+                break;
+            }
+            counter++;
+        }
+
+        if (counter == r.size())
+        {
+            cout << a << ";" << b << endl;
+            return false;
+        }
+    }
+
+    for (int i = 0; i < r.size() - 1; i++)
+    {
+        Point& a = r[i];
+        Point& b = r[(i + 1) % r.size()];
+        int counter = 0;
+        for (Point& z : l)
+        {
+            if (getTangentDirection(a, b, z) <= 0)
+            {
+                break;
+            }
+            counter++;
+        }
+
+        if (counter == l.size())
+        {
+            cout << a << ";" << b << endl;
+            return false;
+        }
+    }
+
+    cout << endl;
+    return true;
+}
+
+      
+
 
 void uniformPointDistribution()
 {
@@ -216,10 +325,33 @@ void intersectingHulls()
     cout << doConvexHullsIntersect(ch1, ch2) << endl;
 }
 
+void intersectingHullsComplete()
+{
+    //vector<Point> ch1{{-10, -10}, {-9, 4}, {3, 3}, {0, -4}};
+    //vector<Point> ch2{{20, 20}, {25, 10}, {5, 5}, {10, 15}, {17, 19}};
+    //vector<Point> ch1{{-10, -10}, {-9, 4}, {3, 10}, {7, 9}};
+    //vector<Point> ch2{{20, 20}, {25, 10}, {10, 5}, {10, 15}, {17, 19}};
+    vector<Point> ch1{{-10, -10}, {-9, 4}, {3, 10}, {7, 9}};
+    vector<Point> ch2{{20, 20}, {25, 10}, {0, 0}, {10, 15}, {17, 19}};
+    for (auto& p : ch1)
+    {
+        cout << p << ';';
+    }
+    cout << endl;
+
+    for (auto& p : ch2)
+    {
+        cout << p << ';';
+    }
+    cout << endl;
+    cout << doConvexHullsIntersectGuaranteed(ch1, ch2) << endl;
+}
+
 int main()
 {
     //uniformPointDistribution();
     //gaussianPointDistribution();
     //intersectingHulls();
+    intersectingHullsComplete();
     return 0;
 }
