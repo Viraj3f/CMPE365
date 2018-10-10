437eff701bb28e97187ce4928131d5d1f2a73f62
Viraj Bangari <Viraj3f@gmail.com>
2018-10-10 11:25:22 -0400

    Finish lab4

--- lab4/greedy_planes.cpp
+++ lab4/greedy_planes.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
 #include <assert.h>
 #include <fstream>
+#include <random>
+
 
 using std::cout;
 using std::endl;
@@ -70,55 +71,148 @@ void testInputs()
     assert(res == 4);
 }
 
-
-void part1FirstInputs()
+void loadFileIntoVector(int i, vector<PlaneEntry>& pe)
 {
-    std::ifstream start("start1.csv");
-    std::ifstream end("finish1.csv");
+    std::ifstream start("start" + std::to_string(i) + ".csv");
+    std::ifstream end("finish" + std::to_string(i) + ".csv");
     assert(start.is_open() && end.is_open());
 
-    std::vector<PlaneEntry> pe;
     while (!start.eof())
     {
-        float startTime;
-        float endTime;
+        double startTime;
+        double endTime;
         start >> startTime;
         end >> endTime;
         pe.push_back({startTime, endTime});
     }
-
-    start.close();
-    end.close();
-
-    cout << numberOfGatesNeeded(pe) << endl;
 }
 
-void part1SecondInputs()
+
+void part1()
 {
-    std::ifstream start("start2.csv");
-    std::ifstream end("finish2.csv");
-    assert(start.is_open() && end.is_open());
-
-    std::vector<PlaneEntry> pe;
-    while (!start.eof())
+    for (int i = 1; i <= 2; i++)
     {
-        float startTime;
-        float endTime;
-        start >> startTime;
-        end >> endTime;
-        pe.push_back({startTime, endTime});
+        std::vector<PlaneEntry> pe;
+        loadFileIntoVector(i, pe);
+        cout << "Input " << i << ":" << endl;
+        cout << numberOfGatesNeeded(pe) << endl;
     }
-
-    start.close();
-    end.close();
-
-    cout << numberOfGatesNeeded(pe) << endl;
 }
 
+void part2Early(int i)
+{
+    // Earliest possible departure strategy
+    vector<PlaneEntry> pe;
+    loadFileIntoVector(i, pe);
+
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> lateProbability(0, 1);
+
+    cout << "percentLate,maxLateness,gatesNeeded" << endl;
+    for (double percentLate = 0; percentLate <= 1; percentLate += 0.1)
+    {
+        for (double maxLateness = 0; maxLateness <= 6; maxLateness += 0.15)
+        {
+            std::uniform_real_distribution<double> delayTime(0, maxLateness);
+            vector<PlaneEntry> testData;
+            for (PlaneEntry& p : pe)
+            {
+                testData.push_back(p);
+                if (lateProbability(generator) < percentLate)
+                {
+                    double delay = delayTime(generator);
+                    PlaneEntry& plane = testData.back();
+                    plane.startTime += delay;
+                    if (plane.startTime > plane.endTime)
+                    {
+                        plane.endTime = plane.startTime + 0.25;
+                    }
+                }
+            }
+
+            int gatesNeeded = numberOfGatesNeeded(testData);
+            cout << percentLate << "," << maxLateness << "," << gatesNeeded << endl;
+        }
+    }
+}
+
+
+void part2Punish(int i)
+{
+    // Earliest possible departure strategy
+    vector<PlaneEntry> pe;
+    loadFileIntoVector(i, pe);
+
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> lateProbability(0, 1);
+
+    cout << "percentLate,maxLateness,gatesNeeded" << endl;
+    for (double percentLate = 0; percentLate <= 1; percentLate += 0.1)
+    {
+        for (double maxLateness = 0; maxLateness <= 6; maxLateness += 0.15)
+        {
+            std::uniform_real_distribution<double> delayTime(0, maxLateness);
+            vector<PlaneEntry> testData;
+            for (PlaneEntry& p : pe)
+            {
+                testData.push_back(p);
+                if (lateProbability(generator) < percentLate)
+                {
+                    double delay = delayTime(generator);
+                    PlaneEntry& plane = testData.back();
+                    plane.startTime += delay;
+                    plane.endTime += delay;
+                }
+            }
+
+            int gatesNeeded = numberOfGatesNeeded(testData);
+            cout << percentLate << "," << maxLateness << "," << gatesNeeded << endl;
+        }
+    }
+}
+
+void part2Hybrid(int i)
+{
+    // Earliest possible departure strategy
+    vector<PlaneEntry> pe;
+    loadFileIntoVector(i, pe);
+
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> lateProbability(0, 1);
+
+    cout << "percentLate,maxLateness,gatesNeeded" << endl;
+    for (double percentLate = 0; percentLate <= 1; percentLate += 0.1)
+    {
+        for (double maxLateness = 0; maxLateness <= 6; maxLateness += 0.15)
+        {
+            std::uniform_real_distribution<double> delayTime(0, maxLateness);
+            vector<PlaneEntry> testData;
+            for (PlaneEntry& p : pe)
+            {
+                testData.push_back(p);
+                if (lateProbability(generator) < percentLate)
+                {
+                    double delay = delayTime(generator);
+                    PlaneEntry& plane = testData.back();
+                    plane.startTime += delay;
+                    if (plane.startTime > plane.endTime)
+                    {
+                        plane.endTime = plane.startTime + delay;
+                    }
+                }
+            }
+
+            int gatesNeeded = numberOfGatesNeeded(testData);
+            cout << percentLate << "," << maxLateness << "," << gatesNeeded << endl;
+        }
+    }
+}
 
 int main()
 {
-    testInputs();
-    part1FirstInputs();
-    part1SecondInputs();
+    //testInputs();
+    //part1();
+    //part2Early(2);
+    //part2Punish(2);
+    part2Hybrid(2);
 }
